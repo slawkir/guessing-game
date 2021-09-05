996369e8ec46587a73b27eb145631ba7c68a7a80
slawkir <slawkir1984@gmail.com>
2021-09-05 18:21:25 +0400

    class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.lastGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.lastGuess = Math.round(this.min + (this.max - this.min) / 2);
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.max = this.lastGuess;
     }
 
     greater() {
-
+        this.min = this.lastGuess;
     }
 }
 
